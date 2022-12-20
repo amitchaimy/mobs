c92240fb815a7c154194d5b56384ede2707d9154
Amit Chaimy <amit.chaimy2@gmail.com>
2022-12-20 07:03:19 +0200

    initial commit

---
+++ map/map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="map" tilewidth="64" tileheight="64" tilecount="112" columns="14">
+ <image source="../img/Preview.png" width="918" height="515"/>
+</tileset>
